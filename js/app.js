20d7e61279690bcfb76217c81a5dbb04aabb568b
Anniken S <anniken.sandoy@xeris.no>
2024-11-08 16:42:38 +0100

    responsivity changes and mail-button

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function toggleHiddenContent(id) {
+  // Get the clicked element
+  const hiddenElement = document.getElementById(id);
+
+  // Find the parent card of the clicked icon
+  const teamCard = hiddenElement.closest(".teamMember");
+
+  // Hide any other visible content within this team member card
+  const otherExpandedElements = teamCard.querySelectorAll(
+    ".iconWrapper.expanded",
+  );
+  otherExpandedElements.forEach((element) => {
+    if (element !== hiddenElement) {
+      element.classList.remove("expanded");
+    }
+  });
+
+  // Toggle the visibility of the clicked element
+  hiddenElement.classList.toggle("expanded");
+}
